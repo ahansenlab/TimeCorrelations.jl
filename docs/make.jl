782266d6e3196ac4080612d332be6825e3c2b990
= <=>
2024-11-07 11:22:26 -0500

    Added favicon

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,10 @@
 using Documenter, TimeCorrelations
 
 # push!(LOAD_PATH,"~/.julia/dev/TimeCorrelations.jl/src/")
-makedocs(sitename="TimeCorrelations.jl")
+makedocs(sitename="TimeCorrelations.jl",
+	 format=Documenter.HTML(assets=["assets/favicon.ico"]),
+	 pages=["Home" => "index.md"]
+)
 
 deploydocs(
     repo = "github.com/ahansenlab/TimeCorrelations.jl.git",
